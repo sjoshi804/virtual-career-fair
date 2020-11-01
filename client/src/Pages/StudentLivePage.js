0d365935ce4b94d7faaccdb3354ffb76ff987078
denise-wang <denisew2212@gmail.com>
2020-11-01 15:28:36 -0800

    updated student live career fair page layout with cards

--- client/src/Pages/StudentLivePage.js
+++ client/src/Pages/StudentLivePage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Row, Col, Container, Card, Button, CardGroup, Image} from "react-bootstrap";
+import {Card, CardDeck} from "react-bootstrap";
 
 export default class StudentLivePage extends React.Component {
     handleRoute = route => () => {
@@ -7,8 +7,222 @@ export default class StudentLivePage extends React.Component {
         };
   render() {
     return (
-      <div style={{ "background-color": "#84849A", color: "white", "text-align": "center", "height": "100vh" }}>
-          HELLO
+      <div style={{ "text-align": "center", "margin": "20px 20px" }}>
+        <CardDeck>
+        <Card>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+            <Card.Title>Google</Card.Title>
+            <Card.Text>
+                1
+                <br></br>
+                2
+                <br></br>
+                3
+                <br></br>
+                4
+                <br></br>
+                5
+                <br></br>
+                6
+                <br></br>
+            </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+            <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+        </Card>
+        <Card>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+            <Card.Title>Microsoft</Card.Title>
+            <Card.Text>
+                1
+                <br></br>
+                2
+                <br></br>
+                3
+                <br></br>
+                4
+                <br></br>
+                5
+                <br></br>
+                6
+                <br></br>
+            </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+            <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+        </Card>
+        <Card>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+            <Card.Title>Facebook</Card.Title>
+            <Card.Text>
+                1
+                <br></br>
+                2
+                <br></br>
+                3
+                <br></br>
+                4
+                <br></br>
+                5
+                <br></br>
+                6
+                <br></br>
+            </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+            <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+        </Card>
+        </CardDeck>
+        <br></br>
+        <CardDeck>
+        <Card>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+            <Card.Title>Apple</Card.Title>
+            <Card.Text>
+                1
+                <br></br>
+                2
+                <br></br>
+                3
+                <br></br>
+                4
+                <br></br>
+                5
+                <br></br>
+                6
+                <br></br>
+            </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+            <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+        </Card>
+        <Card>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+            <Card.Title>Tesla</Card.Title>
+            <Card.Text>
+                1
+                <br></br>
+                2
+                <br></br>
+                3
+                <br></br>
+                4
+                <br></br>
+                5
+                <br></br>
+                6
+                <br></br>
+            </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+            <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+        </Card>
+        <Card>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+            <Card.Title>Snapchat</Card.Title>
+            <Card.Text>
+                1
+                <br></br>
+                2
+                <br></br>
+                3
+                <br></br>
+                4
+                <br></br>
+                5
+                <br></br>
+                6
+                <br></br>
+            </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+            <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+        </Card>
+        </CardDeck>
+        <br></br>
+        <CardDeck>
+        <Card>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+            <Card.Title>Qualcomm</Card.Title>
+            <Card.Text>
+                1
+                <br></br>
+                2
+                <br></br>
+                3
+                <br></br>
+                4
+                <br></br>
+                5
+                <br></br>
+                6
+                <br></br>
+            </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+            <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+        </Card>
+        <Card>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+            <Card.Title>PayPal</Card.Title>
+            <Card.Text>
+                1
+                <br></br>
+                2
+                <br></br>
+                3
+                <br></br>
+                4
+                <br></br>
+                5
+                <br></br>
+                6
+                <br></br>
+            </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+            <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+        </Card>
+        <Card>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+            <Card.Title>Netflix</Card.Title>
+            <Card.Text>
+                1
+                <br></br>
+                2
+                <br></br>
+                3
+                <br></br>
+                4
+                <br></br>
+                5
+                <br></br>
+                6
+                <br></br>
+            </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+            <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+        </Card>
+        </CardDeck>
       </div>
     );
   }
