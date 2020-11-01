5f99ab298cd1e3b9b6fceb7ba3d9307945b6a821
Siddharth Joshi <sjoshi804@gmail.com>
2020-11-01 09:32:03 +0530

    cleanup

--- server/src/apps/app1/routes.ts
+++
@@ -1,9 +0,0 @@
-import express = require('express');
-import {DummyClass} from './class';
-
-const router = express.Router();
-const dummy = new DummyClass(1);
-
-router.get('/', dummy.do());
-
-export = router;
