b4b77357ab20ba12c54afcc839c8ed8bae3cffb9
Siddharth Joshi <sjoshi804@gmail.com>
2020-11-01 09:35:33 +0530

    added getter & setters, added dummy applicant class

---
+++ server/src/apps/applicant/applicant.ts
@@ -0,0 +1,11 @@
+// Dummy class to write job class
+class Applicant
+{
+    private firstName: string;
+    public constructor(firstName: string)
+    {
+        this.firstName = firstName;
+    }
+}
+
+export { Applicant };
--- server/src/apps/job/job.ts
+++ server/src/apps/job/job.ts
@@ -1,4 +1,5 @@
 import { Company } from '../company/company'
+import { Applicant } from '../applicant/applicant'
 class Job
 {
     private id: string;
@@ -8,6 +9,65 @@ class Job
     private preferredMajors: Array<string>;
     private startDate: Date;
     private applicationEndDate: Date;
+
+    // Getters and Setters
+    public getId(): string {
+        return this.id;
+    }
+
+    public setId(id: string): void {
+        this.id = id;
+    }
+
+    public getTitle(): string {
+        return this.title;
+    }
+
+    public setTitle(title: string): void {
+        this.title = title;
+    }
+
+    public getCompany(): Company {
+        return this.company;
+    }
+
+    public setCompany(company: Company): void {
+        this.company = company;
+    }
+
+    public getDescription(): string {
+        return this.description;
+    }
+
+    public setDescription(description: string): void {
+        this.description = description;
+    }
+
+    public getPreferredMajors(): Array<string> {
+        return this.preferredMajors;
+    }
+
+    public setPreferredMajors(preferredMajors: Array<string>): void {
+        this.preferredMajors = preferredMajors;
+    }
+
+    public getStartDate(): Date {
+        return this.startDate;
+    }
+
+    public setStartDate(startDate: Date): void {
+        this.startDate = startDate;
+    }
+
+    public getApplicationEndDate(): Date {
+        return this.applicationEndDate;
+    }
+
+    public setApplicationEndDate(applicationEndDate: Date): void {
+        this.applicationEndDate = applicationEndDate;
+    }
+
+    private applicants: Array<Applicant>;
 
     public constructor(id: string, title: string, company: Company, description?: string, preferredMajors?: Array<string>, startDate?: Date, applicationEndDate?: Date)
     {
@@ -19,6 +79,10 @@ class Job
         this.applicationEndDate = applicationEndDate;
     }
 
+    public apply(applicant: Applicant)
+    {
+        this.applicants.push(applicant);
+    }
 }
 
 export { Job };
