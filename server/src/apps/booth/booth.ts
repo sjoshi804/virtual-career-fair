153785768e59227ae7f706fd503baedc70c0dfcf
Siddharth Joshi <sjoshi804@gmail.com>
2020-11-02 17:15:09 +0530

    Added booth and meeting

---
+++ server/src/apps/booth/booth.ts
@@ -0,0 +1,35 @@
+import { Meeting } from "../meeting/meeting";
+import { Queue } from "../queue/queue";
+import { Recruiter } from "../user/recruiter"
+import { User } from "../user/user"
+
+class Booth
+{
+    private queue: Queue;
+    private meetings: Array<Meeting>;
+
+    public constructor()
+    {
+        this.queue = new Queue();
+        this.meetings = new Array<Meeting>();
+    }
+
+    public getCompany()
+    {
+
+    }
+
+    public startMeetingWithNextApplicant(recruiters: Array<Recruiter>)
+    {
+        var nextApplicant = this.queue.dequeue();
+        var participants = new Array<User>(nextApplicant);
+        recruiters.forEach(recruiter => {
+            participants.push(recruiter)
+        });
+
+        var meeting = new Meeting(participants);
+
+    }
+}
+
+export { Booth };
