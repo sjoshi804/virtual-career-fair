2b7413a5476e64cfe14d4d29d4a2c915cd3a8539
Siddharth Joshi <sjoshi804@gmail.com>
2020-11-04 14:51:42 +0530

    DBClient abstracts out mongo db connection

--- server/src/.config.ts
+++ server/src/.config.ts
@@ -1,2 +1,3 @@
 export const production = ""
 export const databaseURL = process.env.NODE_ENV === "production" ? "" : "mongodb://localhost:27017";
+export const databaseName = "jobz-main";
---
+++ server/src/apps/db/dbClient.ts
@@ -0,0 +1,21 @@
+import { MongoClient, Db } from "mongodb";
+import config = require("../../.config")
+
+class DBClient {
+    public db: Db;
+    public mongoClient: MongoClient
+    public connect()
+    {
+        MongoClient.connect(config.databaseURL, { useUnifiedTopology: true })
+        .then(function (mongoClient) {
+            this.db = mongoClient.db(config.databaseName)
+            this.mongoClient = mongoClient; 
+        })
+        .catch(function(err) {
+            console.log(err)
+            process.exit(1)
+        })
+    }
+}
+
+export = new DBClient();
