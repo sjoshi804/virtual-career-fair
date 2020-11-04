5454b293914fd1c809401b64ef86e84555aa425a
denise-wang <denisew2212@gmail.com>
2020-11-04 00:02:14 -0800

    added live career fairs component card to recruiter profile page

--- client/src/Pages/RecruiterProfilePage.js
+++ client/src/Pages/RecruiterProfilePage.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import { Row, Col, Container, Card, Button, CardGroup, Image} from "react-bootstrap";
+import { Row, Col, Container, Card, Button, CardGroup, Image} from "react-bootstrap";
 // import profile from '../Images/profile.jpg'; 
 
 export default class RecruiterProfilePage extends React.Component {
@@ -8,8 +8,19 @@ export default class RecruiterProfilePage extends React.Component {
         };
   render() {
     return (
-      <div style={{ "background-color": "white", color: "white", "text-align": "center", "color": "black" }}>
-       HELLO!!!
+        <div>
+      <div style={{ "background-color": "white", color: "white", "text-align": "center", "color": "black", "padding": "20px" }}>
+        <Card className="text-center" style={{"box-shadow": "0 4px 8px 0 rgba(0,0,0,0.2)"}}>
+        <Card.Header><h3>Live Career Fairs</h3></Card.Header>
+        <Card.Body>
+            <Card.Title>Career Fair #1</Card.Title>
+            <Card.Text>
+                This career fair is for the UCLA Engineering Fair on October 21, 2020 from 11AM-2PM PST.
+            </Card.Text>
+            <Button variant="outline-success">Join Now</Button>
+        </Card.Body>
+        </Card>
+        </div>
       </div>
     );
   }
