3809478b017e53f2331a3266f965467351da4fda
yinggez <49324614+yinggez@users.noreply.github.com>
2020-11-05 15:01:40 -0800

    organizer pages (#39)
    * organizer
    
    * rm node_module, add gitignore
    
    * fixed some errors
    
    * updated styles and layouts for organization login page
    
    * updated Manage Fair layout
    
    Chnaged a couple of layout syles for the organizer
    
    Co-authored-by: denise-wang <denisew2212@gmail.com>

--- client/src/Pages/HomePage.js
+++ client/src/Pages/HomePage.js
@@ -1,10 +1,14 @@
 import React from "react";
 import {Button} from "react-bootstrap";
+import LoginPage from "./LoginPage"
+import { Route, Switch } from "react-router-dom";
 
 export default class HomePage extends React.Component {
+
     handleRoute = route => () => {
         this.props.history.push({ pathname: route });
         };
+
   render() {
     return (
     <div>
@@ -41,9 +45,15 @@ export default class HomePage extends React.Component {
             Organizers view their live, upcoming, or past career fairs. They can edit existing careers fairs or create a new career fair by filling out a form with details for the event.
           </h4>
         </p>
-        <Button variant="light">Get Started</Button>
+        <Button variant="light" onClick={this.handleRoute("/login")}>Get Started</Button>
       </div>
+      <Switch>
+         <Route exact path="/login" component={LoginPage} />
+      </Switch>
     </div>
+    
     );
   }
 }
+
+
--- client/src/Pages/index.js
+++ client/src/Pages/index.js
@@ -1,8 +1,14 @@
 import StudentProfilePage from "./StudentProfilePage";
 import StudentLivePage from "./StudentLivePage";
 import HomePage from "./HomePage";
+import LoginPage from "./LoginPage";
+import OrganizerPage from "./OrganizerPage";
+import CreateFairPage from "./CreateFairPage";
+import ManageFairPage from "./ManageFairPage"
 import SearchResultsPage from "./SearchResultsPage";
 import RecruiterProfilePage from "./RecruiterProfilePage";
 import RouteNotFound from "./RouteNotFound";
 
-export { StudentProfilePage, HomePage, SearchResultsPage, RouteNotFound, StudentLivePage, RecruiterProfilePage };
+
+export { StudentProfilePage, HomePage, SearchResultsPage, RouteNotFound, StudentLivePage, RecruiterProfilePage, ManageFairPage, CreateFairPage, OrganizerPage, LoginPage };
+
