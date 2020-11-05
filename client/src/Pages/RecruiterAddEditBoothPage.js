b5f34fd852e9de4253f7b68fd1b5273670e0db4c
denise-wang <denisew2212@gmail.com>
2020-11-04 23:05:13 -0800

    updated input form to edit existing booth on recruiter add/edit booth page

--- client/src/Pages/RecruiterAddEditBoothPage.js
+++ client/src/Pages/RecruiterAddEditBoothPage.js
@@ -11,8 +11,8 @@ export default class RecruiterAddEditBoothPage extends React.Component {
     return (
         <div style={{"padding": "20px"}}>
             <Card style={{"box-shadow": "8px 4px 8px 4px rgba(0,0,0,0.2)", "padding": "20px"}}>
-                <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
-                <Tab eventKey="home" title="Add New Booth">
+                <Tabs defaultActiveKey="home" id="uncontrolled-tab-example">
+                <Tab eventKey="new" title="Add New Booth">
                     <br></br>
                     <h2>Add New Booth at Harvey Mudd College Computer Science Fair</h2>
                     <Card style={{"margin-top": "20px"}}>
@@ -68,13 +68,87 @@ export default class RecruiterAddEditBoothPage extends React.Component {
                                 <FormControl as="textarea" aria-label="With textarea" placeholder="Enter additional details" />
                             </InputGroup>
                             <div style={{"margin": "auto", "width": "50px", "padding-top": "15px"}}>
-                            <Button variant="outline-primary">Save</Button>
+                            <Button variant="outline-primary" onClick={this.handleRoute("/recruiter")}>Save</Button>
                             </div>
                         </div>
                     </Card>
                 </Tab>
-                <Tab eventKey="profile" title="Profile">
-                    PROFILE
+                <Tab eventKey="home" title="Edit Existing Booth">
+                    <br></br>
+                    <h2>Edit Existing Booth at Harvey Mudd College Computer Science Fair</h2>
+                    <Card style={{"margin-top": "20px"}}>
+                        <Card>
+                        <Card.Body>
+                            <Card.Title>Edit Company</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted"><b>Current:</b> Google</Card.Subtitle>
+                            <InputGroup size="sm" className="mb-3">
+                                <InputGroup.Prepend>
+                                <InputGroup.Text id="inputGroup-sizing-sm">Edit Company</InputGroup.Text>
+                                </InputGroup.Prepend>
+                                <FormControl aria-label="Edit company name" aria-describedby="inputGroup-sizing-sm" />
+                            </InputGroup>
+                            <Card.Link href="#">Save</Card.Link>
+                        </Card.Body>
+                        </Card>
+
+                        <Card>
+                        <Card.Body>
+                            <Card.Title>Edit Role(s)</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted"><b>Current:</b> Seeking software engineer interns.</Card.Subtitle>
+                            <InputGroup size="sm" className="mb-3">
+                                <InputGroup.Prepend>
+                                <InputGroup.Text id="inputGroup-sizing-sm">Edit Role(s)</InputGroup.Text>
+                                </InputGroup.Prepend>
+                                <FormControl aria-label="Edit role(s)" aria-describedby="inputGroup-sizing-sm" />
+                            </InputGroup>
+                            <Card.Link href="#">Save</Card.Link>
+                        </Card.Body>
+                        </Card>
+
+                        <Card>
+                        <Card.Body>
+                            <Card.Title>Edit Target Year(s)</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted"><b>Current:</b> Sophomores and Juniors. This internship is intended for students who are pursuing a Bachelor's degree program in Computer Science or a related field with an anticipated graduation date after December 2021, depending on their program and unique circumstances.</Card.Subtitle>
+                            <InputGroup size="sm" className="mb-3">
+                                <InputGroup.Prepend>
+                                <InputGroup.Text id="inputGroup-sizing-sm">Edit Target Year(s)</InputGroup.Text>
+                                </InputGroup.Prepend>
+                                <FormControl as="textarea" aria-label="Edit target year(s)" aria-describedby="inputGroup-sizing-sm" />
+                            </InputGroup>
+                            <Card.Link href="#">Save</Card.Link>
+                        </Card.Body>
+                        </Card>
+
+                        <Card>
+                        <Card.Body>
+                            <Card.Title>Edit Requirements</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted"><b>Current: </b> • Pursuing a Bachelor's degree program or post secondary or training experience with a focus on subjects in software development or other technical related field.
+                                • Experience in Software Development and coding in a general purpose programming language.
+                                • Experience coding in two or more of C, C++, Java, JavaScript, Python, or similar.</Card.Subtitle>
+                            <InputGroup size="sm" className="mb-3">
+                                <InputGroup.Prepend>
+                                <InputGroup.Text id="inputGroup-sizing-sm">Edit Requirement(s)</InputGroup.Text>
+                                </InputGroup.Prepend>
+                                <FormControl as="textarea" aria-label="Edit requirement(s)" aria-describedby="inputGroup-sizing-sm" />
+                            </InputGroup>
+                            <Card.Link href="#">Save</Card.Link>
+                        </Card.Body>
+                        </Card>
+
+                        <Card>
+                        <Card.Body>
+                            <Card.Title>Edit Description</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted"><b>Current: </b>Join us for a 12-14 week paid internship that offers personal and professional development, an executive speaker series, and community-building. The Software Engineering Internship program will give you an opportunity to work on complex computer science solutions, develop scalable, distributed software systems, and also collaborate on multitudes of smaller projects that have universal appeal.</Card.Subtitle>
+                            <InputGroup size="sm" className="mb-3">
+                                <InputGroup.Prepend>
+                                <InputGroup.Text>Edit Description: </InputGroup.Text>
+                                </InputGroup.Prepend>
+                                <FormControl as="textarea" placeholder="Edit description" />
+                            </InputGroup>
+                            <Card.Link href="#">Save</Card.Link>
+                        </Card.Body>
+                        </Card>
+                    </Card>
                 </Tab>
                 </Tabs>
             </Card>
