973693b33f0a08257e067bdf8e696cdc667ecf3a
Siddharth Joshi <sjoshi804@gmail.com>
2020-11-08 22:08:08 +0530

    getters & setters for affiliated Organization

--- server/src/apps/user/organizer.ts
+++ server/src/apps/user/organizer.ts
@@ -2,7 +2,16 @@ import { User } from './user'
 
 class Organizer extends User
 {
-    public affiliatedOrganization: string;
+    private affiliatedOrganization: string;
+
+    public getAffiliatedOrganization(): string {
+        return this.affiliatedOrganization;
+    }
+
+    public setAffiliatedOrganization(affiliatedOrganization: string): void {
+        this.affiliatedOrganization = affiliatedOrganization;
+    }
+
 
     public constructor(name: string, emailID: string, password: string, token: string, affiliatedOrganization: string)
     {
