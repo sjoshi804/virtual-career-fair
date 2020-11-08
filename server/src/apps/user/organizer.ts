3face37109e2cd1eab5106a972ee4c8ddf60bf34
Siddharth Joshi <sjoshi804@gmail.com>
2020-11-08 21:38:32 +0530

    Organizer added affiliated organizaiton field

--- server/src/apps/user/organizer.ts
+++ server/src/apps/user/organizer.ts
@@ -2,9 +2,12 @@ import { User } from './user'
 
 class Organizer extends User
 {
-    public constructor(name: string)
+    public affiliatedOrganization: string;
+
+    public constructor(name: string, emailID: string, password: string, token: string, affiliatedOrganization: string)
     {
-        super(name)
+        super(name, emailID, password, token);
+        this.affiliatedOrganization = affiliatedOrganization;
     }
 }
 
