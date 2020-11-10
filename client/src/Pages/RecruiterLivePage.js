fc5db74bdfcab8cfedcaa7a3f6e45038748f7aa8
denise-wang <denisew2212@gmail.com>
2020-11-09 23:37:14 -0800

    updated student images, button links to resume, disabled position buttons

--- client/src/Pages/RecruiterLivePage.js
+++ client/src/Pages/RecruiterLivePage.js
@@ -1,15 +1,6 @@
 import React from "react";
-import {Card, CardDeck, Button} from "react-bootstrap";
-import google from '../Images/google.jpg'; 
-import microsoft from '../Images/microsoft.jpg'; 
-import facebook from '../Images/facebook.jpg'; 
-import apple from '../Images/apple.jpg'; 
-import tesla from '../Images/tesla.jpg'; 
-import snapchat from '../Images/snapchat.jpg'; 
-import qualcomm from '../Images/qualcomm.jpg'; 
-import paypal from '../Images/paypal.jpg'; 
-import netflix from '../Images/netflix.jpg'; 
-import {MoreInfo} from './MoreInfo'
+import {Card, CardDeck, Button, Image} from "react-bootstrap";
+import profile from '../Images/profile.jpg'; 
 
 export default class RecruiterLivePage extends React.Component {
     handleRoute = route => () => {
@@ -18,81 +9,88 @@ export default class RecruiterLivePage extends React.Component {
   render() {
     return (
       <div style={{padding: "20px", "text-align": "center"}}>
-        <Card style={{"padding": "20px"}}>
+        <Card style={{"padding": "20px", "box-shadow": "8px 4px 8px 4px rgba(0,0,0,0.2)"}}>
             <h3><b>Live Career Fair:</b> UCLA Engineering Tech Fair</h3>
             <h6>Students in queue for Google full-time software engineering roles.</h6>
         </Card>
-        <Card style={{padding: "20px"}}>
+        <br></br>
+        <Card style={{padding: "20px", "box-shadow": "8px 4px 8px 4px rgba(0,0,0,0.2)"}}>
             <Card>
                 <Card.Header as="h5">Position 1</Card.Header>
                 <Card.Body>
-                    <Card.Title>Denise Wang</Card.Title>
+                    <Image src={profile} rounded height="150px"/>
+                    <Card.Title style={{"margin-top": "10px"}}>Denise Wang</Card.Title>
                     <Card.Text>
-                    Student bio if they have one: 
+                    Student bio if they have one.
                     </Card.Text>
-                    <Button size="sm" variant="outline-success">Join Now</Button>
+                    <Button size="sm" variant="outline-success" href="https://meet.google.com/ezb-yrqf-vsq">Join Now</Button>
                     &nbsp;&nbsp;&nbsp;&nbsp;
-                    <Button size="sm" variant="outline-dark">View Resume</Button>
+                    <Button size="sm" variant="outline-dark" href="https://writing.colostate.edu/guides/documents/resume/functionalSample.pdf">View Resume</Button>
                 </Card.Body>
             </Card>
             <Card>
                 <Card.Header as="h5">Position 2</Card.Header>
                 <Card.Body>
-                    <Card.Title>Siddharth Joshi</Card.Title>
+                    <Image src={profile} rounded height="150px"/>
+                    <Card.Title style={{"margin-top": "10px"}}>Siddharth Joshi</Card.Title>
                     <Card.Text>
-                    With supporting text below as a natural lead-in to additional content.
+                    Student bio if they have one.
                     </Card.Text>
-                    <Button size="sm" variant="outline-dark">Position 2</Button>
+                    <Button size="sm" variant="outline-secondary" disabled style={{ pointerEvents: 'none' }}>Position 2</Button>
                     &nbsp;&nbsp;&nbsp;&nbsp;
-                    <Button size="sm" variant="outline-dark">View Resume</Button>
+                    <Button size="sm" variant="outline-dark" href="https://writing.colostate.edu/guides/documents/resume/functionalSample.pdf">View Resume</Button>
                 </Card.Body>
             </Card>
             <Card>
                 <Card.Header as="h5">Position 3</Card.Header>
                 <Card.Body>
-                    <Card.Title>Arnav Garg</Card.Title>
+                    <Image src={profile} rounded height="150px"/>
+                    <Card.Title style={{"margin-top": "10px"}}>Arnav Garg</Card.Title>
                     <Card.Text>
-                    With supporting text below as a natural lead-in to additional content.
+                    Student bio if they have one.
                     </Card.Text>
-                    <Button size="sm" variant="outline-dark">Position 3</Button>
+                    <Button size="sm" variant="outline-secondary" disabled style={{ pointerEvents: 'none' }}>Position 3</Button>
                     &nbsp;&nbsp;&nbsp;&nbsp;
-                    <Button size="sm" variant="outline-dark">View Resume</Button>
+                    <Button size="sm" variant="outline-dark" href="https://writing.colostate.edu/guides/documents/resume/functionalSample.pdf">View Resume</Button>
                 </Card.Body>
             </Card>
             <Card>
                 <Card.Header as="h5">Position 4</Card.Header>
                 <Card.Body>
-                    <Card.Title>Yingge Zhou</Card.Title>
+                    <Image src={profile} rounded height="150px"/>
+                    <Card.Title style={{"margin-top": "10px"}}>Yingge Zhou</Card.Title>
                     <Card.Text>
-                    With supporting text below as a natural lead-in to additional content.
+                    Student bio if they have one.
                     </Card.Text>
-                    <Button size="sm" variant="outline-dark">Position 4</Button>
+                    <Button size="sm" variant="outline-secondary" disabled style={{ pointerEvents: 'none' }}>Position 4</Button>
                     &nbsp;&nbsp;&nbsp;&nbsp;
-                    <Button size="sm" variant="outline-dark">View Resume</Button>
+                    <Button size="sm" variant="outline-dark" href="https://writing.colostate.edu/guides/documents/resume/functionalSample.pdf">View Resume</Button>
                 </Card.Body>
             </Card>
             <Card>
                 <Card.Header as="h5">Position 5</Card.Header>
                 <Card.Body>
-                    <Card.Title>Gautam Nambiar</Card.Title>
+                    <Image src={profile} rounded height="150px"/>
+                    <Card.Title style={{"margin-top": "10px"}}>Gautam Nambiar</Card.Title>
                     <Card.Text>
-                    With supporting text below as a natural lead-in to additional content.
+                    Student bio if they have one.
                     </Card.Text>
-                    <Button size="sm" variant="outline-dark">Position 5</Button>
+                    <Button size="sm" variant="outline-secondary" disabled style={{ pointerEvents: 'none' }}>Position 5</Button>
                     &nbsp;&nbsp;&nbsp;&nbsp;
-                    <Button size="sm" variant="outline-dark">View Resume</Button>
+                    <Button size="sm" variant="outline-dark" href="https://writing.colostate.edu/guides/documents/resume/functionalSample.pdf">View Resume</Button>
                 </Card.Body>
             </Card>
             <Card>
                 <Card.Header as="h5">Position 6</Card.Header>
                 <Card.Body>
-                    <Card.Title>Jayant Mehra</Card.Title>
+                    <Image src={profile} rounded height="150px"/>
+                    <Card.Title style={{"margin-top": "10px"}}>Jayant Mehra</Card.Title>
                     <Card.Text>
-                    With supporting text below as a natural lead-in to additional content.
+                    Student bio if they have one.
                     </Card.Text>
-                    <Button size="sm" variant="outline-dark">Position 6</Button>
+                    <Button size="sm" variant="outline-secondary" disabled style={{ pointerEvents: 'none' }}>Position 6</Button>
                     &nbsp;&nbsp;&nbsp;&nbsp;
-                    <Button size="sm" variant="outline-dark">View Resume</Button>
+                    <Button size="sm" variant="outline-dark" href="https://writing.colostate.edu/guides/documents/resume/functionalSample.pdf">View Resume</Button>
                 </Card.Body>
             </Card>
         </Card>
