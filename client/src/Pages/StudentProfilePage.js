f8a4128c883e8edc6265917a09f3619549e60f00
denise-wang <denisew2212@gmail.com>
2020-11-11 21:36:07 -0800

    updating upcoming career fairs (not registered) component

--- client/src/Pages/StudentProfilePage.js
+++ client/src/Pages/StudentProfilePage.js
@@ -84,6 +84,46 @@ export default class StudentProfilePage extends React.Component {
                 </CardGroup>
             </Card>
             </div>
+            <div style={{"padding": "20px", "color": "black"}}>
+                <Card style={{"padding": "20px", "box-shadow": "0 4px 8px 0 rgba(0,0,0,0.2)" }}>
+                <Card.Header><h2><b>Upcoming Career Fairs (Not Registered)</b></h2></Card.Header>
+                    <CardGroup style={{ "width": "100%", "padding": "0 20px 20px 20px", "padding-top": "20px"}}>
+                        <Card style={{ "padding-bottom": "5px"}}>
+                            <Card.Header><h3>Career Fair #5</h3></Card.Header>
+                            <Card.Body>
+                            <Card.Title><b>Status: </b>Not Registered</Card.Title>
+                            <Card.Text>
+                                <p><b>Date: </b>Thursday, November 5, 2020</p>
+                                <p><b>Time: </b> 11 PST - 3PM PST</p>
+                                <Button variant="outline-dark" onClick={this.handleRoute("/student-upcoming")}>View Fair</Button>
+                            </Card.Text>
+                            </Card.Body>
+                        </Card>
+                        <Card>
+                            <Card.Header><h3>Career Fair #6</h3></Card.Header>
+                            <Card.Body>
+                            <Card.Title><b>Status: </b> Not Registered</Card.Title>
+                            <Card.Text>
+                                <p><b>Date: </b>Wednesday, November 11, 2020</p>
+                                <p><b>Time: </b> 11AM PST - 5PM PST</p>
+                                <Button variant="outline-dark" onClick={this.handleRoute("/student-upcoming")}>View Fair</Button>
+                            </Card.Text>
+                            </Card.Body>
+                        </Card>
+                        <Card>
+                            <Card.Header><h3>Career Fair #7</h3></Card.Header>
+                            <Card.Body>
+                            <Card.Title><b>Status: </b> Not Registered</Card.Title>
+                            <Card.Text>
+                                <p><b>Date: </b>Tuesday, November 17, 2020</p>
+                                <p><b>Time: </b>10AM PST - 3PM PST</p>
+                                <Button variant="outline-dark" onClick={this.handleRoute("/student-upcoming")}>View Fair</Button>
+                            </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </CardGroup>
+                </Card>
+            </div>
       </div>
     );
   }
