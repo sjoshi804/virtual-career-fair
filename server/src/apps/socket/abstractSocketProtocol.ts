70aa6b0a704921eed046204a369295ac891f9690
Siddharth Joshi <sjoshi804@gmail.com>
2020-11-20 22:59:07 +0530

    Abstract Socket Protocol Completed. (#85)

--- server/src/apps/socket/abstractSocketProtocol.ts
+++ server/src/apps/socket/abstractSocketProtocol.ts
@@ -7,14 +7,40 @@ abstract class AbstractSocketProtocol implements ISocketProtocol
 
     protected namespace: any;
 
+    protected connectedClients: Array<string>;
+
+    private initConnectedClients()
+    {
+        if (this.connectedClients == undefined)
+        {
+            this.connectedClients = new Array<string>();
+        }
+    }
+
     public onConnection(socket: any) 
     {
-        console.log(`${this.protocolName}: ${socket.id} connected.`);
+        this.initConnectedClients();
+        if (this.connectedClients.indexOf(socket.id) == -1)
+        {
+            console.log(`${this.protocolName}: ${socket.id} connected.`);
+        }
+        else
+        {
+            console.log(`${this.protocolName}: ${socket.id} already connected.`);
+        }
     }
 
     public onDisconnection(socket: any) 
     {
-        console.log(`${this.protocolName}: ${socket.id} disconnected.`);
+        this.initConnectedClients();
+        if (this.connectedClients.indexOf(socket.id) == -1)
+        {
+            console.log(`${this.protocolName}: ${socket.id} was never connected, but attempted to disconnect. Possible error.`);
+        }
+        else
+        {
+            console.log(`${this.protocolName}: ${socket.id} disconnected.`);
+        }
     }
 
     public echo(socket, message)
