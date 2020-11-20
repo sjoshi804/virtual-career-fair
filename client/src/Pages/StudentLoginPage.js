e714a020c67323d9dc020ee3b78c398ea2c73e6b
denise-wang <denisew2212@gmail.com>
2020-11-19 21:27:31 -0800

    finished sign up form for student login page

--- client/src/Pages/StudentLoginPage.js
+++ client/src/Pages/StudentLoginPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Tab , Tabs, Card, Form, Button} from "react-bootstrap";
+import { Tab , Tabs, Card, Form, Button, Col} from "react-bootstrap";
 import profile from '../Images/profile.jpg'; 
 // import profile from '../Images/profile.jpg'; 
 
@@ -13,6 +13,7 @@ export default class StudentLoginPage extends React.Component {
                 <Card style={{"box-shadow": "8px 4px 8px 4px rgba(0,0,0,0.2)", "padding": "20px", "max-width": "80%", "margin": "auto"}}>
                     <Tabs defaultActiveKey="signin" id="uncontrolled-tab-example" style={{"margin": "auto"}}>
                         <Tab eventKey="signin" title="Sign In">
+                        <h3>Sign In</h3> 
                         <Form style={{"padding-top": "10px"}}>
                             <Form.Group controlId="formBasicEmail">
                                 <Form.Label>Email address</Form.Label>
@@ -21,7 +22,6 @@ export default class StudentLoginPage extends React.Component {
                                 We'll never share your email with anyone else.
                                 </Form.Text>
                             </Form.Group>
-
                             <Form.Group controlId="formBasicPassword">
                                 <Form.Label>Password</Form.Label>
                                 <Form.Control type="password" placeholder="Password" />
@@ -35,6 +35,39 @@ export default class StudentLoginPage extends React.Component {
                         </Form>
                         </Tab>
                         <Tab eventKey="signup" title="Sign Up">
+                        <h3>Create an account</h3> 
+                        <Form style={{"padding-top": "10px"}}>
+                            <Form>
+                            <Form.Row>
+                                <Col>
+                                First name
+                                <Form.Control placeholder="First name" />
+                                </Col>
+                                <Col>
+                                Last name
+                                <Form.Control placeholder="Last name" />
+                                </Col>
+                            </Form.Row>
+                            </Form>
+                            <br></br>
+                            <Form.Group controlId="formBasicEmail">
+                                <Form.Label>Email address</Form.Label>
+                                <Form.Control type="email" placeholder="Enter email" />
+                                <Form.Text className="text-muted">
+                                We'll never share your email with anyone else.
+                                </Form.Text>
+                            </Form.Group>
+                            <Form.Group controlId="formBasicPassword">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control type="password" placeholder="Password" />
+                            </Form.Group>
+                            <Form.Group controlId="formBasicCheckbox">
+                                <Form.Check type="checkbox" label="Remember me" />
+                            </Form.Group>
+                            <Button variant="primary" type="submit" onClick={this.handleRoute("/student")}>
+                                Submit
+                            </Button>
+                        </Form>
                         </Tab>
                     </Tabs>
                 </Card>
