bfa19476072bcf283af02118a4da17677156d380
denise-wang <denisew2212@gmail.com>
2020-11-25 21:47:35 -0800

    added notes component to recruiter video call page

--- client/src/Pages/RecruiterVideoCall.js
+++ client/src/Pages/RecruiterVideoCall.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Row, Col, Container, Card, Button, CardGroup, Image} from "react-bootstrap";
+import { Card, Button, CardGroup, Image, Form} from "react-bootstrap";
 
 export default class RecruiterVideoCall extends React.Component {
     handleRoute = route => () => {
@@ -8,12 +8,36 @@ export default class RecruiterVideoCall extends React.Component {
   render() {
     return (
         <div style={{ "color": "black", "margin": "auto"}}>
-            <video style={{ "margin": "20px"}} width="480" height="360" controls>
-                <source src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" type="video/mp4"/>
-            </video>
-            <video style={{ "margin": "20px"}} width="480" height="360" controls>
-                <source src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" type="video/mp4"/>
-            </video>
+        <div className="text-center" style={{ "color": "black", "padding": "20px"}}>
+            <Card style={{"box-shadow": "0 4px 8px 0 rgba(0,0,0,0.2)", "padding": "20px"}}>
+                <h2>Video Call With Student Applicant</h2> 
+            </Card>
+        </div>
+        <div style={{ "color": "black", "padding": "20px"}}>
+            <Card style={{"box-shadow": "0 4px 8px 0 rgba(0,0,0,0.2)"}}>
+                <div style={{"display": "inline"}}>
+                    <div style={{"display": "inline-block"}}>
+                        <video style={{ "margin": "20px"}} width="480" height="360" controls>
+                            <source src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" type="video/mp4"/>
+                        </video>
+                    </div>
+                    <div style={{"display": "inline-block"}}>
+                        <video style={{ "margin": "20px"}} width="480" height="360" controls>
+                            <source src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" type="video/mp4"/>
+                        </video>
+                    </div>
+                </div>
+            </Card>
+        </div>
+        <div className="text-center" style={{ "color": "black", "padding": "20px"}}>
+            <Card style={{"box-shadow": "0 4px 8px 0 rgba(0,0,0,0.2)", "padding": "20px"}}>
+                <h2>Notes</h2>
+                <Form.Group style={{"width": "80%", "margin": "auto"}} controlId="exampleForm.ControlTextarea1">
+                            <Form.Control as="textarea" rows={6} />
+                </Form.Group>
+                <Button size="sm" variant="outline-dark" style={{"width": "50px", "margin": "auto", "margin-top": "20px" }}>Save</Button>  
+            </Card>
+        </div>
         </div>
     );
   }
