f583943a44004d36e09c2fc4d10052ed62c21a1f
denise-wang <42252682+denise-wang@users.noreply.github.com>
2020-11-29 05:16:49 -0800

    92 Recruiter Page- Video Call/Notes Page (#100)
    * added boilerplate for recruiter video call page
    
    * added boilerplates for recruiter and student videos
    
    * added notes component to recruiter video call page

--- client/src/Main.js
+++ client/src/Main.js
@@ -4,7 +4,7 @@ import { Navbar, Nav, Form, Button } from "react-bootstrap";
 
 import { StudentProfilePage, HomePage, SearchResultsPage, RouteNotFound, StudentLivePage, RecruiterProfilePage, ManageFairPage, CreateFairPage, OrganizerPage, OrganizerLivePage, 
   OrganizerPastPage,
-  OrganizerUpcomingPage, LoginPage, RecruiterAddEditBoothPage, RecruiterLivePage, StudentUpcomingPage, SearchPage, StudentPastPage, RecruiterPastPage, StudentLoginPage, RecruiterLoginPage} from "./Pages";
+  OrganizerUpcomingPage, LoginPage, RecruiterAddEditBoothPage, RecruiterLivePage, StudentUpcomingPage, SearchPage, StudentPastPage, RecruiterPastPage, StudentLoginPage, RecruiterLoginPage, RecruiterVideoCall} from "./Pages";
 
 
 class Main extends React.Component {
@@ -71,6 +71,7 @@ class Main extends React.Component {
           <Route exact path="/student-past" component={StudentPastPage} />
           <Route exact path="/student-login" component={StudentLoginPage} />
           <Route exact path="/recruiter-login" component={RecruiterLoginPage} />
+          <Route exact path="/recruiter-video-call" component={RecruiterVideoCall} />
           <Route component={RouteNotFound} />
         </Switch>
       </>
