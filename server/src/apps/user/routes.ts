e95129718b92b7496bbd1c46e49fee4fd5314416
Arnav Garg <arnavgrg@gmail.com>
2020-11-29 22:14:44 -0800

    login endpoint consistent with signup

--- server/src/apps/user/routes.ts
+++ server/src/apps/user/routes.ts
@@ -13,7 +13,7 @@ const UserRouter = express.Router();
 UserRouter.post("/login", async (req, res) => {
 
     const filterQuery = {
-        _id: req.body._id
+        emailId: req.body.emailId
     }
     
     // Check if user exists in the database
@@ -22,18 +22,21 @@ UserRouter.post("/login", async (req, res) => {
     if (user != null) {
         // Instantiate User Object
         let currentUser = new User(user.userType, user.name, user.emailId,
-                                 user.password, "");
+                                 user.password);
         // Ensure the password is the same
-        if (currentUser.getPassword() == req.body.password){
+        if (currentUser.getPassword() == req.body.password) {
             // Send token
             var token = currentUser.getToken();
-            res.status(200).send({token});
+            res.status(200).send({
+                "AuthorizationToken": token
+            });
         }
         // Return unauthorized error code
         else {
             res.sendStatus(401);
         }      
-    } 
+    }
+    
     // Did not find the item in the database
     else {
         res.sendStatus(404);
