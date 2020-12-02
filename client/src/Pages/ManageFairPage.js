a52524c2e27818b79f7ccca56885a9dad01fd8b5
yinggez <49324614+yinggez@users.noreply.github.com>
2020-12-01 20:27:44 -0800

    manage fair page update (#112)
    the user would be able to edit the fair info(time date description) now

--- client/src/Pages/ManageFairPage.js
+++ client/src/Pages/ManageFairPage.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { Card, CardDeck, Button} from "react-bootstrap";
+import { Card, Button, InputGroup, FormControl} from "react-bootstrap";
 
 export default class ManageFairPage extends React.Component {
-    handleroute = routes => () => {
+    handleRoute = routes => () => {
         this.props.history.push({ pathname: routes });
       };
 
@@ -18,206 +18,56 @@ export default class ManageFairPage extends React.Component {
                     <br></br>
                     <h5><b>Time: </b>Live Now, 11 PST - 3PM PST</h5>
                     <br></br>
-                    <Button variant="light"  onClick={this.handleroute("/organization-upcoming")}>Back to Upcoming Fairs</Button>
+                    <Button variant="light" onClick={this.handleRoute("/organizer-upcoming")}>Back to Upcoming Fairs</Button>
                 </Card.Body>
             </Card>
-            <CardDeck>
-            <Card style={{"box-shadow": "0 4px 8px 0 rgba(0,0,0,0.2)"}}>
-                <div style={{ "width": "200px", "margin": "auto"}}> 
-                <Card.Img variant="top" src="holder.js/171x180" height="200" />
-                </div>
-                <Card.Body>
-                <Card.Title>Google</Card.Title>
-                <Card.Text>
+            
                     <br></br>
-                    <b>Recruiter: </b> John Doe
-                    <br></br>
-                    <br></br>
-                    <b>Status: </b> In session
-                </Card.Text>
-                </Card.Body>
-                <Card.Footer>
-                <small className="text-muted"> <h7 style={{"font-size": "15px"}}><b>Students in Queue: </b> 15 </h7> 
-                <h1></h1>
-                <Button variant="outline-secondary" size="sm">Close Booth</Button></small>
-                <br></br>
-                </Card.Footer>
-            </Card>
-            <Card style={{"box-shadow": "0 4px 8px 0 rgba(0,0,0,0.2)"}}>
-                <div style={{ "width": "200px", "margin": "auto", "padding-top": "10px"}}> 
-                <Card.Img variant="top" src="holder.js/171x180" height="200"/>
-                </div>
-                <Card.Body>
-                <Card.Title>Microsoft</Card.Title>
-                <Card.Text>
-                    <br></br>
-                    <b>Recruiter: </b> John Doe
-                    <br></br>
-                    <br></br>
-                    <b>Status: </b> In session
-                </Card.Text>
-                </Card.Body>
-                <Card.Footer>
-                <small className="text-muted"> <h7 style={{"font-size": "15px"}}><b>Students in Queue: </b> 15 </h7> 
-                <h1></h1>
-                <Button variant="outline-dark" size="sm">Close Booth</Button></small>
-                <br></br>
-                </Card.Footer>
-            </Card>
-            <Card style={{"box-shadow": "0 4px 8px 0 rgba(0,0,0,0.2)"}}>
-                <div style={{ "width": "200px", "margin": "auto", "padding-top": "10px"}}> 
-                <Card.Img variant="top" src="holder.js/171x180" height="200"/>
-                </div>
-                <Card.Body>
-                <Card.Title>Facebook</Card.Title>
-                <Card.Text>
-                    <br></br>
-                    <b>Recruiter: </b> John Doe
-                    <br></br>
-                    <br></br>
-                    <b>Status: </b> Available
-                </Card.Text>
-                </Card.Body>
-                <Card.Footer>
-                <small className="text-muted"> <h7 style={{"font-size": "15px"}}><b>Students in Queue: </b> 15 </h7> 
-                <h1></h1>
-                <Button variant="outline-success" size="sm">Close Booth</Button></small>
-                <br></br>
-                </Card.Footer>
-            </Card>
-            </CardDeck>
-            <br></br>
-            <CardDeck>
-            <Card style={{"box-shadow": "0 4px 8px 0 rgba(0,0,0,0.2)"}}>
-                <div style={{ "margin": "auto", "width": "300px"}}> 
-                <Card.Img variant="top" src="holder.js/171x180" height="200px"/>
-                </div>
-                <Card.Body>
-                <Card.Title>Apple</Card.Title>
-                <Card.Text>
-                    <br></br>
-                    <b>Recruiter: </b> John Doe
-                    <br></br>
-                    <br></br>
-                    <b>Status: </b> In session
-                </Card.Text>
-                </Card.Body>
-                <Card.Footer>
-                <small className="text-muted"> <h7 style={{"font-size": "15px"}}><b>Students in Queue: </b> 1</h7> 
-                <h1></h1>
-                <Button variant="outline-secondary" size="sm">Close Booth</Button></small>
-                <br></br>
-                </Card.Footer>
-            </Card>
-            <Card style={{"box-shadow": "0 4px 8px 0 rgba(0,0,0,0.2)"}}>
-                <div style={{ "width": "200px", "margin": "auto", "padding-top": "10px"}}> 
-                <Card.Img variant="top" src="holder.js/171x180" height="200"/>
-                </div>
-                <Card.Body>
-                <Card.Title>Tesla</Card.Title>
-                <Card.Text>
-                    <br></br>
-                    <b>Recruiter: </b> John Doe
-                    <br></br>
-                    <br></br>
-                    <b>Status: </b> In session
-                </Card.Text>
-                </Card.Body>
-                <Card.Footer>
-                <small className="text-muted"> <h7 style={{"font-size": "15px"}}><b>Students in Queue: </b> 404 </h7> 
-                <h1></h1>
-                <Button variant="outline-dark" size="sm">Close Booth</Button></small>
-                <br></br>
-                </Card.Footer>
-            </Card>
-            <Card style={{"box-shadow": "0 4px 8px 0 rgba(0,0,0,0.2)"}}>
-                <div style={{ "width": "200px", "margin": "auto", "padding-top": "10px"}}> 
-                <Card.Img variant="top" src="holder.js/171x180" height="200"/>
-                </div>
-                <Card.Body>
-                <Card.Title>Snapchat</Card.Title>
-                <Card.Text>
-                    <br></br>
-                    <b>Recruiter: </b> John Doe
-                    <br></br>
-                    <br></br>
-                    <b>Status: </b> Available
-                </Card.Text>
-                </Card.Body>
-                <Card.Footer>
-                <small className="text-muted"> <h7 style={{"font-size": "15px"}}><b>Students in Queue: </b> 15 </h7> 
-                <h1></h1>
-                <Button variant="outline-success" size="sm">Close Booth</Button></small>
-                <br></br>
-                </Card.Footer>
-            </Card>
-            </CardDeck>
-            <br></br>
-            <CardDeck>
-            <Card style={{"box-shadow": "0 4px 8px 0 rgba(0,0,0,0.2)"}}>
-                <div style={{ "width": "200px", "margin": "auto", "padding-top": "10px"}}> 
-                <Card.Img variant="top" src="holder.js/171x180" height="200"/>
-                </div>
-                <Card.Body>
-                <Card.Title>Qualcomm</Card.Title>
-                <Card.Text>
-                    <br></br>
-                    <b>Recruiter: </b> John Doe
-                    <br></br>
-                    <br></br>
-                    <b>Status: </b> In session
-                </Card.Text>
-                </Card.Body>
-                <Card.Footer>
-                <small className="text-muted"> <h7 style={{"font-size": "15px"}}><b>Students in Queue: </b> 0 </h7> 
-                <h1></h1>
-                <Button variant="outline-secondary" size="sm">Close Booth</Button></small>
-                <br></br>
-                </Card.Footer>
-            </Card>
-            <Card style={{"box-shadow": "0 4px 8px 0 rgba(0,0,0,0.2)"}}>
-                <div style={{ "width": "200px", "margin": "auto", "padding-top": "10px"}}> 
-                <Card.Img variant="top" src="holder.js/171x180" height="200"/>
-                </div>
-                <Card.Body>
-                <Card.Title>PayPal</Card.Title>
-                <Card.Text>
-                    <br></br>
-                    <b>Recruiter: </b> John Doe
-                    <br></br>
-                    <br></br>
-                    <b>Status: </b> In session
-                </Card.Text>
-                </Card.Body>
-                <Card.Footer>
-                <small className="text-muted"> <h7 style={{"font-size": "15px"}}><b>Students in Queue: </b> 2 </h7> 
-                <h1></h1>
-                <Button variant="outline-dark" size="sm">Close Booth</Button></small>
-                <br></br>
-                </Card.Footer>
-            </Card>
-            <Card style={{"box-shadow": "0 4px 8px 0 rgba(0,0,0,0.2)"}}>
-                <div style={{ "width": "200px", "margin": "auto", "padding-top": "10px"}}> 
-                <Card.Img variant="top" src="holder.js/171x180" />
-                </div>
-                <Card.Body>
-                <Card.Title>Netflix</Card.Title>
-                <Card.Text>
-                    <br></br>
-                    <b>Recruiter: </b> John Doe
-                    <br></br>
-                    <br></br>
-                    <b>Status: </b> Available
-                </Card.Text>
-                </Card.Body>
-                <Card.Footer>
-                <small className="text-muted"> <h7 style={{"font-size": "15px"}}><b>Students in Queue: </b> 15 </h7> 
-                <h1></h1>
-                <Button variant="outline-success" size="sm">Close Booth</Button></small>
-                <br></br>
-                </Card.Footer>
-            </Card>
-            </CardDeck>
+                    <h2>Edit Career Fair # 1</h2>
+                    <Card style={{"margin-top": "20px"}}>
+                        <Card>
+                        <Card.Body>
+                            <Card.Title>Edit Date</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted"><b>Current:</b> Today, October 25, 2020</Card.Subtitle>
+                            <InputGroup size="sm" className="mb-3">
+                                <InputGroup.Prepend>
+                                <InputGroup.Text id="inputGroup-sizing-sm">Edit Date of Career Fair</InputGroup.Text>
+                                </InputGroup.Prepend>
+                                <FormControl type="date" aria-label="Edit Date of Career Fair" aria-describedby="inputGroup-sizing-sm" placeholder="date"/>
+                            </InputGroup>
+                            <Button variant="outline-primary">Save</Button>
+                        </Card.Body>
+                        </Card>
+
+                        <Card>
+                        <Card.Body>
+                            <Card.Title>Edit Time</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted"><b>Current:</b>Live Now, 11 PST - 3PM PST</Card.Subtitle>
+                            <InputGroup size="sm" className="mb-3">
+                                <InputGroup.Prepend>
+                                <InputGroup.Text id="inputGroup-sizing-sm">Edit Career Fair Time</InputGroup.Text>
+                                </InputGroup.Prepend>
+                                <FormControl type="time" aria-label="Edit Career Fair Time" aria-describedby="inputGroup-sizing-sm" />
+                            </InputGroup>
+                            <Button variant="outline-primary">Save</Button>
+                        </Card.Body>
+                        </Card>
+
+                        <Card>
+                        <Card.Body>
+                            <Card.Title>Edit Description</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted"><b>Current: </b>Join us for a 12-14 week paid internship that offers personal and professional development, an executive speaker series, and community-building. The Software Engineering Internship program will give you an opportunity to work on complex computer science solutions, develop scalable, distributed software systems, and also collaborate on multitudes of smaller projects that have universal appeal.</Card.Subtitle>
+                            <InputGroup size="sm" className="mb-3">
+                                <InputGroup.Prepend>
+                                <InputGroup.Text>Edit Description: </InputGroup.Text>
+                                </InputGroup.Prepend>
+                                <FormControl as="textarea" placeholder="Edit description" />
+                            </InputGroup>
+                            <Button variant="outline-primary">Save</Button>
+                        </Card.Body>
+                        </Card>
+                    </Card>
+                
           </div>
         );
   }
