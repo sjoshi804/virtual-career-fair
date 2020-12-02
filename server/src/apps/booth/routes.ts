9967d2039e711420a87c48874147d42b2bf9c3d5
Jayant Mehra <mehrajayant@gmail.com>
2020-12-01 19:36:23 -0800

    booth routes + cf constructor

--- server/src/apps/booth/routes.ts
+++ server/src/apps/booth/routes.ts
@@ -8,13 +8,16 @@
 
 import express = require('express');
 import { CareerFair } from '../careerfair/careerfair';
+import { CareerFairDBSchema } from '../careerFair/careerFairDBSchema';
 import { Booth } from './booth';
+import { BoothDBSchema } from './boothDBSchema';
 
 const BoothRouter = express.Router();
 
 // Create booth associated with this company
-BoothRouter.post("/:careerfairid/company", async (req, res) => {
+BoothRouter.post("/:careerfairid/company/:companyId", async (req, res) => {
     const careerfairid = req.params.careerfairid;
+    
     // Get career fair and make sure it exists
     const filterQuery = {
         _id: careerfairid
@@ -22,10 +25,10 @@ BoothRouter.post("/:careerfairid/company", async (req, res) => {
     var careerfair = await CareerFair.db.findOne(filterQuery);
     if (careerfair != null) {
         // If this company does not already have a booth in this career fair
-        if (!(req.body.companyId in careerfair.booths)) {
+        if (!(req.params.companyId in careerfair.booths)) {
             // TODO: Validate Company ID to make sure this company exists
             // Create DBSchema object using request body
-            const booth = new Booth(null, careerfairid, req.body.companyId);
+            const booth = new Booth(null, careerfairid, req.params.companyId);
             careerfair.booths[booth.getCompanyId()] = booth
             const updateQuery = {
                 $set: careerfair
@@ -51,11 +54,56 @@ BoothRouter.post("/:careerfairid/company", async (req, res) => {
 });
 
 BoothRouter.get("/:careerfairid/company/:companyid", async (req, res) => {
-
+    const careerfairid = req.params.careerfairid;
+    // Get career fair and make sure it exists
+    const filterQuery = {
+        _id: careerfairid
+    }
+    var careerfair = await CareerFair.db.findOne(filterQuery);
+    if (careerfair != null) {
+        if ( ! (req.params.companyid in careerfair.booths) ) {
+            res.sendStatus(404);
+        }
+        else {
+            res.status(200).send(careerfair.booths[req.params.companyid]);
+        }
+    }
+    else {
+        res.sendStatus(404);
+    }
 });
 
 BoothRouter.delete("/:careerfairid/company/:companyid", async (req, res) => {
+    const careerfairid = req.params.careerfairid;
+    
+    // Get career fair and make sure it exists
+    const filterQuery = {
+        _id: careerfairid
+    }
+    var careerfair = await CareerFair.db.findOne(filterQuery);
+    if (careerfair != null) {
+        if ( ! (req.params.companyid in careerfair.booths) ) {
+            res.sendStatus(404);
+        }
+        else {
+            delete careerfair.booths[req.params.companyid]
 
+            const updateQuery = {
+                $set: careerfair
+            }
+            
+            if (await CareerFair.db.updateOne(filterQuery, updateQuery)) {
+                res.sendStatus(201);
+                return;
+            } else {
+                res.sendStatus(500);
+                return;
+            }
+        }
+    }
+    else {
+        res.sendStatus(404);
+    }
 });
 
 export { BoothRouter };
