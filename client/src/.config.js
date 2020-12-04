ffd4a88629243bcafe5ac9ba56482e4c274e8dd6
Siddharth Joshi <sjoshi804@gmail.com>
2020-12-05 00:37:53 +0530

    Ngrok config

--- client/src/.config.js
+++ client/src/.config.js
@@ -2,7 +2,7 @@
     Constant base URL to be changed as needed for Heroku deploymnt
 */ 
 
-const domainName = "localhost:3000";
+const domainName = "b544188aa67d.ngrok.io";
 
 export const baseUrl = "http://" + domainName + "/api";
 export const socketBaseUrl = "ws://" + domainName;
